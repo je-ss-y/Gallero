c9ab311dec343538d8a9bdee987c39f50eb84607
je-ss-y <Nkujess@gmail.com>
2019-10-11 11:09:43 +0200

    bootstrapped my pages

--- pictures/views.py
+++ pictures/views.py
@@ -1,8 +1,36 @@
-from django.shortcuts import render
-from django.http  import HttpResponse
+from django.shortcuts import render, redirect
+from django.http  import HttpResponse,Http404
+import datetime as dt
 
 
 # Create your views here.
 
 def welcome(request):
-    return HttpResponse('Welcome to My Gallery')
+    return render(request, 'welcome.html')
+
+
+def pictures_of_day(request):
+    date = dt.date.today()
+    return render(request, 'all-posts/pictures-today.html', {"date": date,})
+
+
+
+
+def past_days_pictures(request, past_date):
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(news_of_day)
+
+    return render(request, 'all-posts/past-pictures.html', {"date": date})
+
+
+
