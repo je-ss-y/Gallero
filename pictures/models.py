5004055d5f40e0674e6ce24e8bed0c8345e7c09b
je-ss-y <Nkujess@gmail.com>
2019-10-11 12:12:15 +0200

    added functions to update and delete entries from database

--- pictures/models.py
+++ pictures/models.py
@@ -6,3 +6,10 @@ from django.db import models
 class Category(models.Model):
     category_name = models.CharField(max_length =30)
 
+
+    def __str__(self):
+        return self.category_name
+
+        class Meta:
+        ordering = ['category_name']
+
